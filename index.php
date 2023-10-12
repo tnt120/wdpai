f3047ac242639cd070f56fd079564947710c8a5c
Artur Pajor <apaj04@gmail.com>
2023-10-12 14:55:05 +0200

    Added simple html to php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Application</title>
+</head>
+<body>
+    <p>This is my html</p>
+</body>
+</html>
+
 <?php
 
     echo 'Hi there 👋';
