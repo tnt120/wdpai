4a1b4601379e2f844567a32b4ea9b21a22555a3c
Artur Pajor <apaj04@gmail.com>
2023-11-22 13:55:05 +0100

    [feat] added basic php routing for login and homepage

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bookify</title>
-</head>
-<body>
-    <p>Bookify application</p>
-</body>
-</html>
-
 <?php
 
-    echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('login', 'DefaultController');
+Routing::get('home', 'DefaultController');
+Routing::run($path);
 
 ?>
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+
+    public function login() {
+        $this->render('login');
+    }
+
+    public function home() {
+        $this->render('home');
+    }
+}
+
+?>
