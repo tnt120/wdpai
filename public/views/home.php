0471a71d3f542f1c455b39a9096f0632524d897a
tnt120 <apaj04@gmail.com>
2024-01-17 01:27:10 +0100

    [refactor] changed search icon

--- public/views/home.php
+++ public/views/home.php
@@ -5,8 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/css/home.css">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script type="text/javascript" src="./public/js/search.js" defer></script>
     <script type="text/javascript" src="./public/js/redirect.js"></script>
     <title>Home</title>
@@ -56,7 +54,7 @@
                     <?php endforeach; ?>
                 </select>
             </div>
-            <button type="button" class="primary-button"><span class="material-symbols-outlined">search</span></button>
+            <button type="button" class="primary-button"><img src="/public/img/search.svg" alt="search"></button>
         </div>
         <section class="content">
             <?php foreach ($books as $book): ?>
